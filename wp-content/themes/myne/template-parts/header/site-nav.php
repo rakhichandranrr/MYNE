679ffd2655f0b9edc4370fbd96f070ebe68043a0
Sooraj <sooraj.srishti@gmail.com>
2024-05-06 11:34:40 +0530

    hide

--- wp-content/themes/myne/template-parts/header/site-nav.php
+++ wp-content/themes/myne/template-parts/header/site-nav.php
@@ -25,6 +25,7 @@ if (strpos($url,'price') !== false) {
     <li class="nav-item"> <a class="nav-link <?php if(is_page('features')){?>active<?php } ?>" aria-current="page" href="<?php echo site_url();?>/features/">Features</a> </li>
     <li class="nav-item"> <a class="nav-link <?php echo $cls;?>" href="<?php echo site_url();?>/#price" id="pricemenu">Pricing</a> </li>
     <li class="nav-item"> <a class="nav-link <?php if(is_page('about-us')){?>active<?php } ?>" href="<?php echo site_url();?>/about-us/">Security</a> </li>
+    <!-- login-start -->
     <li class="nav-item dropdown btn-Link mobHide"> <a class="nav-link dropdown-toggle btn btn-login" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Log in</a>
       <ul class="dropdown-menu">
         <li class="nav-item"> <a class="nav-link" href="<?php echo $header['login_customer_url'];?>">Customer</a> </li>
@@ -33,11 +34,17 @@ if (strpos($url,'price') !== false) {
     </li>
     <li class="nav-item btn-Link mobOnly"> <a class="nav-link btn btn-login" href="<?php echo $header['log_in_as_customer_mobile_url'];?>">Log in as Customer</a> </li>
     <li class="nav-item btn-Link mobOnly"> <a class="nav-link btn btn-login" href="<?php echo $header['log_in_as_wealth_planner_mobile_url'];?>">Log in as Wealth Planner</a> </li>
-    <li class="nav-item dropdown btn-Link"> <a class="nav-link dropdown-toggle btn" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Get Started</a> 
+    <!-- login-End -->
+
+    <!-- Signup-Start -->
+    <li class="nav-item dropdown btn-Link mobHide"> <a class="nav-link dropdown-toggle btn" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Get Started</a> 
       <ul class="dropdown-menu signUp">
         <li class="nav-item"> <a class="nav-link" href="<?php echo $header['get_started_url'];?>">Sign up for client user </a> </li>
         <li class="nav-item"> <a class="nav-link" href="<?php echo $header['login_wealth_planner_url'];?>">Sign up page</a> </li>
       </ul>
     </li>
+    <li class="nav-item btn-Link mobOnly"> <a class="nav-link btn btn-login" href="<?php echo $header['log_in_as_customer_mobile_url'];?>">Sign up for client user</a> </li>
+    <li class="nav-item btn-Link mobOnly"> <a class="nav-link btn btn-login" href="<?php echo $header['log_in_as_wealth_planner_mobile_url'];?>">Sign up page</a> </li>
+    <!-- Signup-End -->
   </ul>
 </div>
